538ad340f4e0c780d420ff85b022eb67c82c4c22
aphkyle <aphkyle@gmail.com>
2023-09-17 23:30:51 +0800

    fix more bugs caused by my bug fixing

--- main.js
+++ main.js
@@ -3,24 +3,69 @@ let quad = document.getElementById("quad")
 let inp = document.getElementById("inp")
 let submit = document.getElementById("sub")
 
+function gcd(a,b) {
+    a = Math.abs(a);
+    b = Math.abs(b);
+    if (b > a) {var temp = a; a = b; b = temp;}
+    while (true) {
+        if (b == 0) return a;
+        a %= b;
+        if (a == 0) return b;
+        b %= a;
+    }
+}
+
 let genRand = (a, b, c, d) => {
     // does cool stuff like (ax + by)(cx + dy) and expands it
-    let ax = Math.ceil(Math.random() * a)
-    let by = Math.ceil(Math.random() * b) * (Math.sign(Math.random()-0.5))
-    let cx = Math.ceil(Math.random() * c)
-    let dy = Math.ceil(Math.random() * d) * (Math.sign(Math.random()-0.5))
+    let ax = Math.floor(Math.random() * a) + 1
+    let by = (Math.floor(Math.random() * b) + 1) * (Math.sign(Math.random()-0.5))
+    let cx = Math.floor(Math.random() * c) + 1
+    let dy = (Math.floor(Math.random() * d) + 1) * (Math.sign(Math.random()-0.5))
+
+    dn = gcd(ax, by)
+    en = gcd(cx, dy)
 
     console.log(ax, by, cx, dy)
-    ans.push(`(${ax}x+${by}y)(${cx}x+${dy}y)`.replace(/(^|[^0-9])1([xy]{1,2})/, '').replace(/(\+\-|\-\+)/g, "-"))
-    ans.push(`(${by}y+${ax}x)(${dy}y+${cx}x)`.replace(/(^|[^0-9])1([xy]{1,2})/, '').replace(/(\+\-|\-\+)/g, "-"))
-    ans.push(`(${cx}x+${dy}y)(${ax}x+${by}y)`.replace(/(^|[^0-9])1([xy]{1,2})/, '').replace(/(\+\-|\-\+)/g, "-"))
-    ans.push(`(${dy}y+${cx}x)(${by}y+${ax}x)`.replace(/(^|[^0-9])1([xy]{1,2})/, '').replace(/(\+\-|\-\+)/g, "-"))
-    let res = `${ax*cx}x^2 + ${ax*dy + by*cx}xy + ${by * dy}y^2`.replace(/(^|[^0-9])1([xy]{1,2})/, '').replace(/(\+\-|\-\+)/g, "-")
+    console.log(dn, en)
+
+    if (dn * en === 1){
+        ans.push(`(${ax}x+${by}y)(${cx}x+${dy}y)`.replace(/(?<!\d)1([xy])/g, "$1").replace(/(\+\-|\-\+)/g, "-"))
+        ans.push(`(${by}y+${ax}x)(${dy}y+${cx}x)`.replace(/(?<!\d)1([xy])/g, "$1").replace(/(\+\-|\-\+)/g, "-"))
+        ans.push(`(${cx}x+${dy}y)(${ax}x+${by}y)`.replace(/(?<!\d)1([xy])/g, "$1").replace(/(\+\-|\-\+)/g, "-"))
+        ans.push(`(${dy}y+${cx}x)(${by}y+${ax}x)`.replace(/(?<!\d)1([xy])/g, "$1").replace(/(\+\-|\-\+)/g, "-"))
+    } else if (dn === 1){
+        cx = Math.floor(cx/en)
+        dy = Math.floor(dy/en)
+        ans.push(`${en}(${ax}x+${by}y)(${cx}x+${dy}y)`.replace(/(?<!\d)1([xy])/g, "$1").replace(/(\+\-|\-\+)/g, "-"))
+        ans.push(`${en}(${by}y+${ax}x)(${dy}y+${cx}x)`.replace(/(?<!\d)1([xy])/g, "$1").replace(/(\+\-|\-\+)/g, "-"))
+        ans.push(`${en}(${cx}x+${dy}y)(${ax}x+${by}y)`.replace(/(?<!\d)1([xy])/g, "$1").replace(/(\+\-|\-\+)/g, "-"))
+        ans.push(`${en}(${dy}y+${cx}x)(${by}y+${ax}x)`.replace(/(?<!\d)1([xy])/g, "$1").replace(/(\+\-|\-\+)/g, "-"))
+    } else if (en === 1){
+        ax = Math.floor(ax/dn)
+        by = Math.floor(by/dn)
+        ans.push(`${dn}(${ax}x+${by}y)(${cx}x+${dy}y)`.replace(/(?<!\d)1([xy])/g, "$1").replace(/(\+\-|\-\+)/g, "-"))
+        ans.push(`${dn}(${by}y+${ax}x)(${dy}y+${cx}x)`.replace(/(?<!\d)1([xy])/g, "$1").replace(/(\+\-|\-\+)/g, "-"))
+        ans.push(`${dn}(${cx}x+${dy}y)(${ax}x+${by}y)`.replace(/(?<!\d)1([xy])/g, "$1").replace(/(\+\-|\-\+)/g, "-"))
+        ans.push(`${dn}(${dy}y+${cx}x)(${by}y+${ax}x)`.replace(/(?<!\d)1([xy])/g, "$1").replace(/(\+\-|\-\+)/g, "-"))
+    } else {
+        ax = Math.floor(ax/dn)
+        by = Math.floor(by/dn)
+        cx = Math.floor(cx/en)
+        dy = Math.floor(dy/en)
+        ans.push(`${dn*en}(${ax}x+${by}y)(${cx}x+${dy}y)`.replace(/(?<!\d)1([xy])/g, "$1").replace(/(\+\-|\-\+)/g, "-"))
+        ans.push(`${dn*en}(${by}y+${ax}x)(${dy}y+${cx}x)`.replace(/(?<!\d)1([xy])/g, "$1").replace(/(\+\-|\-\+)/g, "-"))
+        ans.push(`${dn*en}(${cx}x+${dy}y)(${ax}x+${by}y)`.replace(/(?<!\d)1([xy])/g, "$1").replace(/(\+\-|\-\+)/g, "-"))
+        ans.push(`${dn*en}(${dy}y+${cx}x)(${by}y+${ax}x)`.replace(/(?<!\d)1([xy])/g, "$1").replace(/(\+\-|\-\+)/g, "-"))
+    }
+    let res = `${ax*cx}x^2+${ax*dy + by*cx}xy+${by * dy}y^2`.replace(/(?<!\d)1([xy])/g, "$1").replace(/(\+\-|\-\+)/g, "-")
     if (res.includes("0xy")){
         return genRand(a, b, c, d)
     }
     
-    return `${ax*cx}x^2+${ax*dy + by*cx}xy+${by * dy}y^2`.replace(/(^|[^0-9])1([xy]{1,2})/, '').replace(/(\+\-|\-\+)/g, "-")
+    console.log(ans)
+    console.log(res)
+
+    return res
 }
 
 submit.addEventListener("click", (e) => {
