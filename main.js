ec5735759c3e2e44a88f63b2d8fb2382749c9f36
aphkyle <aphkyle@gmail.com>
2023-09-17 22:55:05 +0800

    fix more bugs, regenerate when 0xy, fix empty ax^2

--- main.js
+++ main.js
@@ -10,13 +10,17 @@ let genRand = (a, b, c, d) => {
     let cx = Math.ceil(Math.random() * c)
     let dy = Math.ceil(Math.random() * d) * (Math.sign(Math.random()-0.5))
 
-    console.log(Math.random())
-    ans.push(`(${ax}x+${by}y)(${cx}x+${dy}y)`.replace(/(1)[xy]{1,2}/, '').replace(/(\+\-|\-\+)/g, "-"))
-    ans.push(`(${by}y+${ax}x)(${dy}y+${cx}x)`.replace(/(1)[xy]{1,2}/, '').replace(/(\+\-|\-\+)/g, "-"))
-    ans.push(`(${cx}x+${dy}y)(${ax}x+${by}y)`.replace(/(1)[xy]{1,2}/, '').replace(/(\+\-|\-\+)/g, "-"))
-    ans.push(`(${dy}y+${cx}x)(${by}y+${ax}x)`.replace(/(1)[xy]{1,2}/, '').replace(/(\+\-|\-\+)/g, "-"))
-    console.log(`${ax*cx}x^2 + ${ax*dy + by*cx}xy + ${by * dy}y^2`.replace(/(1)[xy]{1,2}/, '').replace(/(\+\-|\-\+)/g, "-"))
-    return `${ax*cx}x^2+${ax*dy + by*cx}xy+${by * dy}y^2`.replace(/(1)[xy]{1,2}/, '').replace(/(\+\-|\-\+)/g, "-")
+    console.log(ax, by, cx, dy)
+    ans.push(`(${ax}x+${by}y)(${cx}x+${dy}y)`.replace(/(^|[^0-9])1([xy]{1,2})/, '').replace(/(\+\-|\-\+)/g, "-"))
+    ans.push(`(${by}y+${ax}x)(${dy}y+${cx}x)`.replace(/(^|[^0-9])1([xy]{1,2})/, '').replace(/(\+\-|\-\+)/g, "-"))
+    ans.push(`(${cx}x+${dy}y)(${ax}x+${by}y)`.replace(/(^|[^0-9])1([xy]{1,2})/, '').replace(/(\+\-|\-\+)/g, "-"))
+    ans.push(`(${dy}y+${cx}x)(${by}y+${ax}x)`.replace(/(^|[^0-9])1([xy]{1,2})/, '').replace(/(\+\-|\-\+)/g, "-"))
+    let res = `${ax*cx}x^2 + ${ax*dy + by*cx}xy + ${by * dy}y^2`.replace(/(^|[^0-9])1([xy]{1,2})/, '').replace(/(\+\-|\-\+)/g, "-")
+    if (res.includes("0xy")){
+        return genRand(a, b, c, d)
+    }
+    
+    return `${ax*cx}x^2+${ax*dy + by*cx}xy+${by * dy}y^2`.replace(/(^|[^0-9])1([xy]{1,2})/, '').replace(/(\+\-|\-\+)/g, "-")
 }
 
 submit.addEventListener("click", (e) => {
