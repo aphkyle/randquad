cc7512a77f91849d44d6bbbcbb38dcf2f8e318dc
aphkyle <aphkyle@gmail.com>
2023-09-17 22:46:58 +0800

    fix bug, add negative b/c

--- main.js
+++ main.js
@@ -6,16 +6,17 @@ let submit = document.getElementById("sub")
 let genRand = (a, b, c, d) => {
     // does cool stuff like (ax + by)(cx + dy) and expands it
     let ax = Math.ceil(Math.random() * a)
-    let by = Math.ceil(Math.random() * b)
+    let by = Math.ceil(Math.random() * b) * (Math.sign(Math.random()-0.5))
     let cx = Math.ceil(Math.random() * c)
-    let dy = Math.ceil(Math.random() * d)
+    let dy = Math.ceil(Math.random() * d) * (Math.sign(Math.random()-0.5))
 
     console.log(Math.random())
-    ans.push(`(${ax}x+${by}y)(${cx}x+${dy}y)`)
-    ans.push(`(${by}y+${ax}x)(${dy}y+${ax}x)`)
-    ans.push(`(${cx}x+${dy}y)(${ax}x+${by}y)`)
-    ans.push(`(${dy}y+${cx}x)(${by}y+${ax}x)`)
-    return `${ax*cx}x^2 + ${ax*dy + by*cx}xy + ${by * dy}y^2`
+    ans.push(`(${ax}x+${by}y)(${cx}x+${dy}y)`.replace(/(1)[xy]{1,2}/, '').replace(/(\+\-|\-\+)/g, "-"))
+    ans.push(`(${by}y+${ax}x)(${dy}y+${cx}x)`.replace(/(1)[xy]{1,2}/, '').replace(/(\+\-|\-\+)/g, "-"))
+    ans.push(`(${cx}x+${dy}y)(${ax}x+${by}y)`.replace(/(1)[xy]{1,2}/, '').replace(/(\+\-|\-\+)/g, "-"))
+    ans.push(`(${dy}y+${cx}x)(${by}y+${ax}x)`.replace(/(1)[xy]{1,2}/, '').replace(/(\+\-|\-\+)/g, "-"))
+    console.log(`${ax*cx}x^2 + ${ax*dy + by*cx}xy + ${by * dy}y^2`.replace(/(1)[xy]{1,2}/, '').replace(/(\+\-|\-\+)/g, "-"))
+    return `${ax*cx}x^2+${ax*dy + by*cx}xy+${by * dy}y^2`.replace(/(1)[xy]{1,2}/, '').replace(/(\+\-|\-\+)/g, "-")
 }
 
 submit.addEventListener("click", (e) => {
